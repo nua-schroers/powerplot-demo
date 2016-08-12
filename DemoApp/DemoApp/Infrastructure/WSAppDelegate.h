4782087bd19965387fb70deab23dc492eee81baa
Dr. Wolfram Schroers <Wolfram.Schroers@Field-theory.org>
2016-08-12 17:58:06 +0200

    Add administrative files
    + Add primary app delegate and main.m file.
    + Add project and related files.

---
+++ DemoApp/DemoApp/Infrastructure/WSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WSAppDelegate.h
+//  PowerPlot
+//
+//  Created by Dr. Wolfram Schroers on 10/13/13.
+//  Copyright (c) 2013 NuAS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
